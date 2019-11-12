4414361d2c1b0546b61785f87ad6f505d2d01dc3
Gary Coburn <gcoburn@Garys-MacBook-Pro.local>
2019-11-12 13:29:58 -0500

    files in directories

---
+++ lib/components/widgets/nav_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+bottomNavigationBar: new BottomNavigationBar(
+items: [
+new BottomNavigationBarItem(
+icon: const Icon(Icons.star),
+title: new Text('Home'),
+),
+
+new BottomNavigationBarItem(
+icon: const Icon(Icons.star),
+title: new Text('Nav0'),
+),
+
+new BottomNavigationBarItem(
+icon: const Icon(Icons.star),
+title: new Text('Nav1'),
+)
+]
+
+),
